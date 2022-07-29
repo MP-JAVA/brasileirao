c387b4b49c3c76a63c843949593d7676d09922e0
leandro souza <leanddrosouzasilva@gmail.com>
2022-07-29 15:44:16 -0300

    Adicionado atributos e gets na classe compeonato.

--- brasileirao/src/brasileirao/Campeonato.java
+++ brasileirao/src/brasileirao/Campeonato.java
@@ -1,10 +1,51 @@
 package brasileirao;
 
-public class Campeonato {
-	
-	
-	
-	
-	
+import java.util.ArrayList;
 
+public class Campeonato {
+		private ArrayList<Time> classificacao;
+		private ArrayList<Jogador> artilharia;
+		private ArrayList<Partida> tabelaDePartidas;
+		private ArrayList<Time> times;
+		private ArrayList<Treinador> treinadores;
+		private ArrayList<Jogador> jogadores;
+
+		public Campeonato(ArrayList<Time> classificacao, 
+				ArrayList<Jogador> artilharia, 
+				ArrayList<Partida> tabelaDePartidas, 
+				ArrayList<Time> times, 
+				ArrayList<Treinador> treinadores, 
+				ArrayList<Jogador> jogadores) {
+			this.classificacao = classificacao;
+			this.artilharia = artilharia;
+			this.tabelaDePartidas = tabelaDePartidas;
+			this.times = times;
+			this.treinadores = treinadores;
+			this.jogadores = jogadores;
+		}
+		
+		public ArrayList<Time> getClassificacao() {
+			return classificacao; 
+		}
+		
+		public ArrayList<Jogador> getArtilharia(){
+			return artilharia;
+		}
+		
+		public ArrayList<Partida> getTabelaDePartidas() {
+			return tabelaDePartidas;
+		}
+		
+		public ArrayList<Time> getTimes(){
+			return times;
+		}
+		
+		public ArrayList<Treinador> getTreinadores() {
+			return treinadores;
+		}
+		
+		public ArrayList<Jogador> getJogadores() {
+			return jogadores;
+			
+		}
 }

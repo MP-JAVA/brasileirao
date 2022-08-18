54091dd698a4258b53d45daeb6077f0602bdab42
leandro souza <leanddrosouzasilva@gmail.com>
2022-08-18 17:09:48 -0300

    Adicionado tabela de classificação.

--- brasileirao/src/brasileiraoView/ClassificacaoView.java
+++ brasileirao/src/brasileiraoView/ClassificacaoView.java
@@ -1,29 +1,44 @@
 package brasileiraoView;
 
-import java.awt.Font;
+import java.awt.GridLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class ClassificacaoView {
 
-	private static JFrame telaClassi = new JFrame("Classificação");
-	private static JLabel title = new JLabel("Classficação");
+	JFrame tabelaClassificacao;
+	JPanel painelFundo;
+	JTable tabela;
+	JScrollPane barraRolagem;
+	ImageIcon image2;
+
+	Object[][] dados = { { "1", "Palmeiras", "48", "22", "14", "6", "2" },
+			{ "2", "Flamengo", "47", "22", "14", "6", "2" }, { "3", "Corinthians", "46", "22", "14", "6", "2" }, };
+
+	String[] colunas = { "Classificação", "Time", "p", "j", "v", "e", "d" };
 
 	public ClassificacaoView() {
-		telaClassi.setSize(600, 400);
-		telaClassi.setLayout(null);
-		telaClassi.setVisible(true);
-		telaClassi.setResizable(false);
+		tabelaClassificacao = new JFrame("Classificação");
+		painelFundo = new JPanel();
+		painelFundo.setLayout(new GridLayout(1, 1));
+		tabela = new JTable(dados, colunas);
+		barraRolagem = new JScrollPane(tabela);
+		painelFundo.add(barraRolagem);
 
-		telaClassi.setBounds(50, 50, 50, 30);
-		telaClassi.setFont(new Font("fira code", Font.BOLD, 20)); // Estilização da fonte
+		tabelaClassificacao.getContentPane().add(painelFundo);
+		tabelaClassificacao.setSize(600, 300);
+		tabelaClassificacao.setVisible(true);
 
-		telaClassi.add(title);
+		image2 = new ImageIcon("bola.png");
+		tabelaClassificacao.setIconImage(image2.getImage());
 	}
 
 	public static void main(String[] args) {
-		ClassificacaoView classi = new ClassificacaoView();
+		new ClassificacaoView();
 	}
 
 }
--- brasileirao/src/brasileiraoView/PartidaView.java
+++ brasileirao/src/brasileiraoView/PartidaView.java
@@ -2,29 +2,37 @@ package brasileiraoView;
 
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class PartidaView {
 
-	private static JFrame telaPart = new JFrame("Partida");
-	private static JLabel title = new JLabel("Partida");
+	JFrame telaPart;
+	JLabel title;
+	ImageIcon image2;
 
 	public PartidaView() {
+		telaPart = new JFrame("Partida");
+		title = new JLabel("Partida");
+
 		telaPart.setSize(600, 400);
 		telaPart.setLayout(null);
 		telaPart.setVisible(true);
 		telaPart.setResizable(false);
 
-		telaPart.setBounds(50, 50, 100, 30);
-		telaPart.setFont(new Font("fira code", Font.BOLD, 20)); // Estilização da fonte
+		title.setBounds(50, 50, 100, 30);
+		title.setFont(new Font("fira code", Font.BOLD, 20)); // Estilização da fonte
+
+		image2 = new ImageIcon("bola.png");
+		telaPart.setIconImage(image2.getImage());
 
 		telaPart.add(title);
 
 	}
 
 	public static void main(String[] args) {
-		ClassificacaoView partida = new ClassificacaoView();
+		new PartidaView();
 	}
 
 }
